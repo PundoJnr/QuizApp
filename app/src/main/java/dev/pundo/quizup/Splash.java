977091c45511886df90e0cb08facb7e9ccb05caf
ArthurPundo <pundoarthur@gmail.com>
2018-06-25 12:44:32 +0300

    splash screen

--- app/src/main/java/dev/pundo/quizup/Splash.java
+++ app/src/main/java/dev/pundo/quizup/Splash.java
@@ -1,13 +1,26 @@
 package dev.pundo.quizup;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
 
+    public static int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, main.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
